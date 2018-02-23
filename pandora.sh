15ec29363c706f163e5335e4df076fa356cd3dce
Devil505 <devil505linux@gmail.com>
2018-02-23 15:22:13 +0100

    pandora updates

--- pandora.sh
+++ pandora.sh
@@ -8,17 +8,17 @@ set -e
 # Should-Stop:          halt reboot
 # Default-Start:        2 3 4 5
 # Default-Stop:         0 1 6
-# Short-Description: Open Parrot Pandora's box and clean the RAM.
+# Short-Description: Open Solus Pandora's box and clean the RAM.
 ### END INIT INFO
 
 #
 # Lorenzo "EclipseSpark" Faletra <eclipse@frozenbox.org>
-#         Parrot Security OS www.parrotsec.org
+#         Solus Security OS www.Solussec.org
 #                      GNU/GPL v3
 #
 
 function bomb {
-	echo "starting Parrot Pandora's box "
+	echo "starting Solus Pandora's box "
 	sleep 5
     echo "Pandora is dropping caches"
     echo 1024 > /proc/sys/vm/min_free_kbytes
@@ -29,7 +29,7 @@ function bomb {
     echo "Pandora is bombing RAM"
     sdmem -fllv
     echo " - RAM bombed"
-    echo "closing Parrot Pandora's box"
+    echo "closing Solus Pandora's box"
 }
 
 
